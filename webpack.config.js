1426110e97292d8097340cc40711ec801cdd2aeb
Julius Rajala <juliusrajala@gmail.com>
2018-05-18 20:46:08 +0300

    First commit on bit-of-trivia.

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const config = {
+  entry: './index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ },
+    ]
+  }
+};
+
+module.exports = config;
