bffad26b0bee5d2fe2a40e90bb9803d3a742ea93
Julius Rajala <juliusrajala@gmail.com>
2018-06-02 23:16:33 +0300

    Rewrote router to use context.

--- src/core/Router.tsx
+++ src/core/Router.tsx
@@ -1,24 +1,46 @@
 import * as React from 'react';
+import produce from 'immer';
+import history from 'browser-history';
 import { Container } from 'src/ui/styles';
 import { splitRouteParams } from 'src/utils/routes';
-import routes from 'src/core/routes';
 import NotFound from 'src/ui/views/404View';
 
-const Router = () => {
-  console.log(window.location)
-  const url = window.location.pathname;
-  if (!routes.has(url)) return <NotFound />;
-  const route = routes.get(url);
-  const loader = route.loader;
+type RouterState = { url: string; };
+const initialState = { url: '' };
 
-  const RouteComponent = route.component;
-  const props = { loader, params: splitRouteParams(window.location.search) };
+const Context = React.createContext({});
+const { Provider, Consumer } = Context;
 
-  return (
-    <Container>
-      <RouteComponent {...props} />
-    </Container>
-  )
+class Router extends React.Component<any, RouterState> {
+  state: RouterState = produce(
+    initialState, draft => { draft.url = window.location.pathname }
+  );
+
+  action = {
+    goTo: (url) => this.setState(
+      state => setUrl(state, url),
+      () => history(url),
+    )
+  }
+
+  componentDidMount() {
+    history((e, url) => this.setState(state => setUrl(state, url)));
+  }
+
+  render() {
+    const { state, action, props } = this;
+
+    return (
+      <Provider value={{ state, action }}>
+        {props.children}
+      </Provider>
+    );
+  }
+}
+
+
+function setUrl(state: RouterState, url: string) {
+  return produce(state, draft => { draft.url = url });
 }
 
 export default Router;
